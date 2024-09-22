4721f9f54819af1d47204561b3fd34d411ae5174
Ester Trevisan <ester.trevisan@academico.ifpb.edu.br>
2024-09-22 16:53:01 -0300

    :sparkles: feat: adicionando análise de comprovante na intent FazerDoacao

--- server/lambda/controllers/doacao.js
+++ server/lambda/controllers/doacao.js
@@ -1,35 +1,44 @@
 import { handleResponse } from "../utils/response-builder.js";
+import { analyzeImage } from "../utils/analyze-image.js";
 
 export const handleDoacaoIntent = async (event) => {
     const intentName = event.sessionState.intent.name;
     const slots = event.sessionState.intent.slots;
     const inputTranscript = event.inputTranscript;
 
-    // Verifica se o tipo de doação é pix
+    // Verifica se a doação é por pix
     if (intentName === 'FazerDoacao' && slots.TipoDeDoacao.value.interpretedValue === 'pix') {
-        
-        // Verifica se o inputTranscript contém um link de imagem
-        if (inputTranscript && inputTranscript.startsWith('<') && inputTranscript.endsWith('>')) {
-            const potentialUrl = inputTranscript.slice(1, -1); // Remove os caracteres '<' e '>'
-            
-            if (isImageUrl(potentialUrl)) {
-                const successMessage = "Obrigado por enviar o comprovante de imagem!";
-                return handleResponse(event, "Close", null, successMessage); // Usando Close com Fulfilled
-            } else {
-                const retryMessage = "O link enviado não parece ser uma imagem. Por favor, envie um link válido de imagem.";
-                return handleResponse(event, "ElicitSlot", "ImagemComprovante", retryMessage);
-            }
-        } else {
-            const noLinkMessage = "Não foi detectado um link de imagem. Por favor, envie o comprovante de pagamento como um link de imagem.";
-            return handleResponse(event, "ElicitSlot", "ImagemComprovante", noLinkMessage);
+        // Verifica se o valor do Pix foi informado
+        if (!slots.ValorPix || !slots.ValorPix.value || !slots.ValorPix.value.interpretedValue) {
+            const requestMessage = "Por favor, informe o valor da doação via Pix.";
+            return handleResponse(event, "ElicitSlot", "ValorPix", requestMessage);
         }
-    }
-    
-    // Caso seja outro tipo de doação
-    return handleResponse(event, "Close", null, "Obrigado pela sua doação!"); // Usando Close com Fulfilled
-};
 
-// Função para verificar se o URL é uma imagem
-const isImageUrl = (url) => {
-    return (url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+        // Se o valor do Pix foi informado, então processa a imagem (após o valor estar confirmado)
+        if (!slots.ImagemComprovante || !slots.ImagemComprovante.value) {
+
+            const message = "Esta é a chave pix para tranferência: pix@gmail.com. Por favor, envie o comprovante após a tranferência para que seja possível registrar sua doação.";
+            return handleResponse(event, "ElicitSlot", "ImagemComprovante", message);
+        }
+
+        // Análise da imagem via função externa
+        try {
+            const { isValorPixFound, isPixKeyFound } = await analyzeImage(slots.ImagemComprovante.value.interpretedValue, slots.ValorPix.value.interpretedValue);
+
+            // Se ambos, o valor do Pix e a chave Pix forem encontrados na imagem
+            if (isValorPixFound && isPixKeyFound) {
+                const successMessage = "Obrigado por enviar o comprovante de imagem!";
+                return handleResponse(event, "Close", null, successMessage);
+            } else {
+                const errorMessage = "O comprovante enviado não contém as informações necessárias.";
+                return handleResponse(event, "ElicitSlot", "ImagemComprovante", errorMessage);
+            }
+        } catch (error) {
+            const errorMessage = "Houve um erro ao processar a imagem. Tente novamente.";
+            return handleResponse(event, "ElicitSlot", "ImagemComprovante", errorMessage);
+        }
+    } else {
+        const successMessage = "Esse é o número da instituição: (83) 99174-6548. Entre em contato para tratar da doação, obrigado!";
+        return handleResponse(event, "Close", null, successMessage);
+    }
 };
