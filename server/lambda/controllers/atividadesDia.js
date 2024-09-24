8eaa83e4843e75c9996d39bc281e1485cdb1f56d
SilasLeão <leaosilas@gmail.com>
2024-09-24 10:51:04 -0300

    ✨ feat: Atualização do código da intent AtividadesDia

--- server/lambda/controllers/atividadesDia.js
+++ server/lambda/controllers/atividadesDia.js
@@ -3,56 +3,77 @@ import { generateTTS } from "../utils/generate-tts.js"; // Importando a função
 
 export const handleAtividadesDiaIntent = async (event) => {
   let responseMessage = "";
+  const intentName = event.sessionState.intent.name;
+  const slots = event.sessionState.intent.slots;
 
   try {
     // Verifica se a intenção é 'AtividadesDia'
-    if (event.sessionState?.intent?.name === "AtividadesDia") {
-      const today = new Date();
-      const dayOfWeek = today.getDay(); // Retorna 0 (Domingo) a 6 (Sábado)
+    if (intentName === "AtividadesDia") {
+      // Verificação da existência dos slots
+      if (!slots) {
+        return handleResponse(event, "Failed", null, "Nenhum slot encontrado.");
+      }
 
-      // Define a mensagem conforme o dia da semana
-      switch (dayOfWeek) {
-        case 0: // Domingo
-          responseMessage =
-            "Não temos atividades marcadas para hoje por ser fim de semana.";
+      if (
+        !slots.DiaSemana ||
+        !slots.DiaSemana.value ||
+        !slots.DiaSemana.value.interpretedValue
+      ) {
+        const requestMessage =
+          "Por favor, informe o dia da semana que deseja consultar.";
+        return handleResponse(event, "ElicitSlot", "DiaSemana", requestMessage);
+      }
+
+      const DiaDaSemana = slots.DiaSemana.value.interpretedValue;
+
+      switch (DiaDaSemana.toUpperCase()) {
+        case "DOMINGO":
+          responseMessage = "Não temos atividades marcadas para Domingo.";
           break;
-        case 1: // Segunda-feira
+        case "SEGUNDA":
           responseMessage =
-            "Hoje teremos arrumação das 9:00 às 12:00 e curso de bordado das 14:00 às 17:00.";
+            "Segunda teremos arrumação das 9:00 às 12:00 e curso de bordado das 14:00 às 17:00.";
           break;
-        case 2: // Terça-feira
+        case "TERÇA":
           responseMessage =
-            "Hoje teremos um período de visitas das 9:00 às 12:00, curso de crochê das 14:00 às 17:00 e sessão de pilates das 17:00 às 19:00.";
+            "Terça teremos um período de visitas das 9:00 às 12:00, curso de crochê das 14:00 às 17:00 e sessão de pilates das 17:00 às 19:00.";
           break;
-        case 3: // Quarta-feira
+        case "QUARTA":
           responseMessage =
-            "Hoje teremos um período de visitas das 9:00 às 12:00 e curso de artesanato das 14:00 às 17:00.";
+            "Quarta teremos um período de visitas das 9:00 às 12:00 e curso de artesanato das 14:00 às 17:00.";
           break;
-        case 4: // Quinta-feira
+        case "QUINTA":
           responseMessage =
-            "Hoje terá o dia da convivência das 9:00 às 17:00 e sessão de pilates das 17:00 às 19:00.";
+            "Quinta terá o dia da convivência das 9:00 às 17:00 e sessão de pilates das 17:00 às 19:00.";
           break;
-        case 5: // Sexta-feira
+        case "SEXTA":
           responseMessage =
-            "Hoje teremos um período de visitas das 9:00 às 12:00 e curso de pintura das 14:00 às 17:00.";
+            "Sexta teremos um período de visitas das 9:00 às 12:00 e curso de pintura das 14:00 às 17:00.";
           break;
-        case 6: // Sábado
-          responseMessage =
-            "Não temos atividades marcadas para hoje por ser fim de semana.";
+        case "SÁBADO":
+          responseMessage = "Não temos atividades marcadas para Sábado.";
           break;
         default:
-          responseMessage = "Nenhuma atividade agendada para hoje.";
+          responseMessage = "Não entendi.";
       }
 
       // Gera o áudio da resposta
       try {
         const audioUrl = await generateTTS(responseMessage);
 
-        return handleResponse(event, 'Close', null, [responseMessage, audioUrl]);
+        return handleResponse(event, "Close", null, [
+          responseMessage,
+          audioUrl,
+        ]);
       } catch (error) {
         console.log(error);
 
-        return handleResponse(event, 'Close', null, 'Ocorreu um problema ao gerar o áudio da resposta.');
+        return handleResponse(
+          event,
+          "Close",
+          null,
+          "Ocorreu um problema ao gerar o áudio da resposta."
+        );
       }
     }
   } catch (error) {
