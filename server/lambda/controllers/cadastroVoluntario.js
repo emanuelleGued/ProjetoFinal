86d630915f0fdcc5d0f9cd99e1fefa2355052002
https-Luan-Fernandes <jose.luan@academico.ifpb.edu.br>
2024-09-24 21:03:24 -0300

    :construction: atualizando função de cadastro de voluntários

--- server/lambda/controllers/cadastroVoluntario.js
+++ server/lambda/controllers/cadastroVoluntario.js
@@ -1,72 +1,94 @@
 import { handleResponse } from '../utils/response-builder.js';
 import { generateTTS } from '../utils/generate-tts.js';
-import { validateSlots } from '../utils/validations.js';
 import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const handleCadastroVoluntarioIntent = async (event) => {
-    let responseMessage = "Aqui estão as informações que você forneceu: ";
+    let responseMessage = "";
 
     try {
         const slots = event.sessionState.intent.slots;
 
-        // Validação dos slots
-        const validation = validateSlots(slots);
-        if (!validation.valid) {
-            return handleResponse(event, 'ElicitSlot', validation.slot, validation.message);
+        // Verificação da existência dos slots
+        if (!slots) {
+            return handleResponse(event, 'Close', null, 'Nenhum slot encontrado.');
         }
 
+        const {
+            NomeVoluntario, RG1, CPF1, Endereco1, TelefoneCelular, Email
+        } = slots;
+
+        // Verificação do NomeVoluntario
+        if (NomeVoluntario && NomeVoluntario.value) {
+            let nomeSlot = NomeVoluntario.value.originalValue.trim();
+            responseMessage += `Nome do Voluntário: ${nomeSlot}. `;
+        } else {
+            return handleResponse(event, 'ElicitSlot', 'NomeVoluntario', 'Por favor, me informe o nome do voluntário.');
+        }
+
+        // Verificação do RG
+        if (RG1 && RG1.value) {
+            let rgSlot = RG1.value.originalValue.trim();
+            responseMessage += `RG: ${rgSlot}. `;
+        } else {
+            return handleResponse(event, 'ElicitSlot', 'RG1', 'Por favor, informe o RG.');
+        }
+
+        // Verificação do CPF
+        if (CPF1 && CPF1.value) {
+            let cpfSlot = CPF1.value.originalValue.trim();
+            responseMessage += `CPF: ${cpfSlot}. `;
+        } else {
+            return handleResponse(event, 'ElicitSlot', 'CPF1', 'Por favor, informe o CPF.');
+        }
+
+        // Verificação do Endereco
+        if (Endereco1 && Endereco1.value) {
+            let enderecoSlot = Endereco1.value.originalValue.trim();
+            responseMessage += `Endereço: ${enderecoSlot}. `;
+        } else {
+            return handleResponse(event, 'ElicitSlot', 'Endereco1', 'Por favor, informe o endereço.');
+        }
+
+        // Verificação do TelefoneCelular
+        if (TelefoneCelular && TelefoneCelular.value) {
+            let telefoneSlot = TelefoneCelular.value.originalValue.trim();
+            responseMessage += `Telefone Celular: ${telefoneSlot}. `;
+        } else {
+            return handleResponse(event, 'ElicitSlot', 'TelefoneCelular', 'Por favor, informe o telefone celular.');
+        }
+
+        // Verificação do Email
+        if (Email && Email.value) {
+            let emailSlot = Email.value.originalValue.trim();
+            responseMessage += `Email: ${emailSlot}. `;
+        } else {
+            return handleResponse(event, 'ElicitSlot', 'Email', 'Por favor, informe o email.');
+        }
 
         // Criar objeto de dados do voluntário
         const volunteerData = {
-            PK: `voluntario#${slots.CPF1.value.originalValue.trim()}`, // Chave primária
-            NomeVoluntario: slots.NomeVoluntario.value.originalValue.trim(),
-            RG: slots.RG1.value.originalValue.trim(),
-            CPF: slots.CPF1.value.originalValue.trim(),
-            EmissaoRG: slots.EmissaoRG.value.originalValue.trim(),
-            DataNascimento: slots.DataNascimento.value.originalValue.trim(),
-            Endereco: slots.Endereco1.value.originalValue.trim(),
-            Bairro: slots.Bairro.value.originalValue.trim(),
-            TelefoneCelular: slots.TelefoneCelular.value.originalValue.trim(),
-            Email: slots.Email.value.originalValue.trim(),
-            Profissao: slots.Profissao1.value.originalValue.trim(),
-            Formacao: slots.Formacao.value.originalValue.trim(),
-            AreaPretendida: slots.AreaPretendida.value.originalValue.trim(),
-            DiaPretendido: slots.DiaPretendido.value.originalValue.trim(),
-            HorarioPretendido: slots.HorarioPretendido.value.originalValue.trim(),
-            IndicacaoAtividade: slots.IndicacaoAtividade.value.originalValue.trim(),
+            PK: `voluntario#${CPF1.value.originalValue.trim()}`,
+            NomeVoluntario: NomeVoluntario.value.originalValue.trim(),
+            RG: RG1.value.originalValue.trim(),
+            CPF: CPF1.value.originalValue.trim(),
+            Endereco: Endereco1.value.originalValue.trim(),
+            TelefoneCelular: TelefoneCelular.value.originalValue.trim(),
+            Email: Email.value.originalValue.trim(),
         };
-
-        // Se todos os campos estiverem válidos, construa a mensagem de resposta
-        for (const key in volunteerData) {
-            const slotValue = volunteerData[key];
-            responseMessage += `${key.replace(/([A-Z])/g, ' $1')}: ${slotValue}. `;
-        }
 
         // Persistir dados no DynamoDB
         await axios.post(`${process.env.ENDPOINT}/cadastrar_voluntario`, volunteerData);
 
         // Gerar áudio da resposta final
-        const audioUrl = await generateTTS(responseMessage);
-        
-        // Retornar resposta final
-        return {
-            sessionState: {
-                dialogAction: {
-                    type: 'Close', // ou 'ElicitSlot' se você estiver solicitando mais informações
-                },
-                intent: {
-                    name: 'CadastroVoluntario',
-                    state: 'Fulfilled',
-                },
-            },
-            messages: [
-                { contentType: 'PlainText', content: responseMessage },
-                { contentType: 'PlainText', content: audioUrl } // O URL do áudio se necessário
-            ],
-        };
+        const audioUrl = await generateTTS('Seu cadastro foi realizado com sucesso!'+ responseMessage);
+
+        // Retornar a resposta final com o áudio
+        return handleResponse(event, 'Close', null, [responseMessage, audioUrl]);
 
     } catch (error) {
-        console.log(error);
-        return handleResponse(event, 'Failed', null, 'Ocorreu um erro ao processar seu cadastro. Por favor, tente novamente.');
+        console.error(error);
+        return handleResponse(event, 'Close', null, 'Ocorreu um erro ao processar seu cadastro. Por favor, tente novamente.');
     }
 };
