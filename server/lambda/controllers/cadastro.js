3cde8beb959939efbca260f2697470e5bb0bf101
emanuelleGued <manugguedes1701@gmail.com>
2024-09-28 19:58:52 -0300

    :broom: cleanup: removendo slots desnecessarios de cadastro

--- server/lambda/controllers/cadastro.js
+++ server/lambda/controllers/cadastro.js
@@ -12,13 +12,12 @@ export const handleCadastroIntent = async (event) => {
 
         // Verificação da existência dos slots
         if (!slots) {
-            return handleResponse(event, 'Failed', null, 'Nenhum slot encontrado.');
+            return handleResponse(event, 'Failed', null, 'Nenhum dado foi informado. Por favor, tente novamente.');
         }
 
         const {
-            Nome, cpf, DataNascimento, Email, Endereco,
-            Escolaridade, EstadoCivil, Genero, Naturalidade, Profissao,
-            telefone, dataExpedicao, orgaoExpedidor, rg, uf, CondicaoMedica, Acompanhamento
+            Nome, DataNascimento, Email, Endereco, EstadoCivil, Genero,
+            telefone, CondicaoMedica, Acompanhamento
         } = slots;
 
         // Verificação do nome
@@ -26,7 +25,7 @@ export const handleCadastroIntent = async (event) => {
             let nomeSlot = Nome.value.originalValue.trim();
             responseMessage += `Nome: ${nomeSlot}. `;
         } else {
-            return handleResponse(event, 'ElicitSlot', 'Nome', 'Por favor, me informe o nome completo?');
+            return handleResponse(event, 'ElicitSlot', 'Nome', 'Poderia me informar nome completo, por favor?');
         }
 
         // Verificação da data de nascimento
@@ -34,7 +33,7 @@ export const handleCadastroIntent = async (event) => {
             let dataNascimentoSlot = DataNascimento.value.originalValue.trim();
             responseMessage += `Data de Nascimento: ${dataNascimentoSlot}. `;
         } else {
-            return handleResponse(event, 'ElicitSlot', 'DataNascimento', 'Qual a data de nascimento?');
+            return handleResponse(event, 'ElicitSlot', 'DataNascimento', 'Qual é a data de nascimento, por gentileza?');
         }
 
         // Verificação do gênero
@@ -42,7 +41,7 @@ export const handleCadastroIntent = async (event) => {
             let generoSlot = Genero.value.originalValue.trim();
             responseMessage += `Gênero: ${generoSlot}. `;
         } else {
-            return handleResponse(event, 'ElicitSlot', 'Genero', 'Me informe o gênero: feminino ou masculino?');
+            return handleResponse(event, 'ElicitSlot', 'Genero', 'Me informe o genero, por favor?');
         }
 
         // Verificação do estado civil
@@ -50,71 +49,7 @@ export const handleCadastroIntent = async (event) => {
             let estadoCivilSlot = EstadoCivil.value.originalValue.trim();
             responseMessage += `Estado Civil: ${estadoCivilSlot}. `;
         } else {
-            return handleResponse(event, 'ElicitSlot', 'EstadoCivil', 'Qual é o seu estado civil?');
-        }
-
-        // Verificação da naturalidade
-        if (Naturalidade && Naturalidade.value) {
-            let naturalidadeSlot = Naturalidade.value.originalValue.trim();
-            responseMessage += `Naturalidade: ${naturalidadeSlot}. `;
-        } else {
-            return handleResponse(event, 'ElicitSlot', 'Naturalidade', 'Qual é a cidade de nascimento?');
-        }
-
-        // Verificação da escolaridade
-        if (Escolaridade && Escolaridade.value) {
-            let escolaridadeSlot = Escolaridade.value.originalValue.trim();
-            responseMessage += `Escolaridade: ${escolaridadeSlot}. `;
-        } else {
-            return handleResponse(event, 'ElicitSlot', 'Escolaridade', 'Me informe a escolaridade? (Ex: Ensino Médio Completo)');
-        }
-
-        // Verificação da profissão
-        if (Profissao && Profissao.value) {
-            let profissaoSlot = Profissao.value.originalValue.trim();
-            responseMessage += `Profissão: ${profissaoSlot}. `;
-        } else {
-            return handleResponse(event, 'ElicitSlot', 'Profissao', 'Me informe a profissão?');
-        }
-
-        // Verificação do CPF
-        if (cpf && cpf.value) {
-            let cpfSlot = cpf.value.originalValue.trim();
-            responseMessage += `CPF: ${cpfSlot}. `;
-        } else {
-            return handleResponse(event, 'ElicitSlot', 'cpf', 'Digite o CPF:');
-        }
-
-        // Verificação do RG
-        if (rg && rg.value) {
-            let rgSlot = rg.value.originalValue.trim();
-            responseMessage += `RG: ${rgSlot}. `;
-        } else {
-            return handleResponse(event, 'ElicitSlot', 'rg', 'Digite o RG:');
-        }
-
-        // Verificação do órgão expedidor
-        if (orgaoExpedidor && orgaoExpedidor.value) {
-            let orgaoExpedidorSlot = orgaoExpedidor.value.originalValue.trim();
-            responseMessage += `Órgão Expedidor: ${orgaoExpedidorSlot}. `;
-        } else {
-            return handleResponse(event, 'ElicitSlot', 'orgaoExpedidor', 'Qual é o órgão expedidor do seu documento?');
-        }
-
-        // Verificação da UF
-        if (uf && uf.value) {
-            let ufSlot = uf.value.originalValue.trim();
-            responseMessage += `UF: ${ufSlot}. `;
-        } else {
-            return handleResponse(event, 'ElicitSlot', 'uf', 'Digite a UF:');
-        }
-
-        // Verificação da data de expedição
-        if (dataExpedicao && dataExpedicao.value) {
-            let dataExpedicaoSlot = dataExpedicao.value.originalValue.trim();
-            responseMessage += `Data de Expedição: ${dataExpedicaoSlot}. `;
-        } else {
-            return handleResponse(event, 'ElicitSlot', 'dataExpedicao', 'Qual é a data de expedição do seu documento?');
+            return handleResponse(event, 'ElicitSlot', 'EstadoCivil', 'Você poderia me dizer o estado civil, por favor?');
         }
 
         // Verificação do endereço
@@ -122,7 +57,7 @@ export const handleCadastroIntent = async (event) => {
             let enderecoSlot = Endereco.value.originalValue.trim();
             responseMessage += `Endereço: ${enderecoSlot}. `;
         } else {
-            return handleResponse(event, 'ElicitSlot', 'Endereco', 'Qual é o seu endereço?');
+            return handleResponse(event, 'ElicitSlot', 'Endereco', 'Poderia me dizer o endereço, por favor?');
         }
 
         // Verificação do telefone
@@ -130,7 +65,7 @@ export const handleCadastroIntent = async (event) => {
             let telefoneSlot = telefone.value.originalValue.trim();
             responseMessage += `Telefone: ${telefoneSlot}. `;
         } else {
-            return handleResponse(event, 'ElicitSlot', 'telefone', 'Me informe um telefone para contato?');
+            return handleResponse(event, 'ElicitSlot', 'telefone', 'Você pode me passar um número de telefone para contato, por gentileza?');
         }
 
         // Verificação do email
@@ -138,7 +73,7 @@ export const handleCadastroIntent = async (event) => {
             let emailSlot = Email.value.originalValue.trim();
             responseMessage += `Email: ${emailSlot}. `;
         } else {
-            return handleResponse(event, 'ElicitSlot', 'Email', 'Me informe um email para contato?');
+            return handleResponse(event, 'ElicitSlot', 'Email', 'Me informe um email para contato, por favor?');
         }
 
         // Verificação da condição médica
@@ -146,7 +81,7 @@ export const handleCadastroIntent = async (event) => {
             let condicaoMedicaSlot = CondicaoMedica.value.originalValue.trim();
             responseMessage += `Condição Médica: ${condicaoMedicaSlot}. `;
         } else {
-            return handleResponse(event, 'ElicitSlot', 'CondicaoMedica', 'Possui alguma condição médica? (Ex: Diabetes)');
+            return handleResponse(event, 'ElicitSlot', 'CondicaoMedica', 'Possui alguma condição médica que gostaria de compartilhar? (Ex: Diabetes)');
         }
 
         // Verificação do acompanhamento
@@ -154,26 +89,19 @@ export const handleCadastroIntent = async (event) => {
             let acompanhamentoSlot = Acompanhamento.value.originalValue.trim();
             responseMessage += `Acompanhamento: ${acompanhamentoSlot}. `;
         } else {
-            return handleResponse(event, 'ElicitSlot', 'Acompanhamento', 'Tem algum acompanhamento?');
+            return handleResponse(event, 'ElicitSlot', 'Acompanhamento', 'Está em acompanhamento médico?');
         }
 
-        // Criar objeto de dados do idoso
+        // Criar objeto de dados
         const idosoData = {
-            PK: `idoso#${cpf.value.originalValue.trim()}`, // Definindo a chave primária
+            PK: `idoso#${Nome.value.originalValue.trim()}`, // Definindo a chave primária
             Nome: Nome.value.originalValue.trim(),
             DataNascimento: DataNascimento.value.originalValue.trim(),
             Email: Email.value.originalValue.trim(),
             Endereco: Endereco.value.originalValue.trim(),
-            Escolaridade: Escolaridade.value.originalValue.trim(),
             EstadoCivil: EstadoCivil.value.originalValue.trim(),
             Genero: Genero.value.originalValue.trim(),
-            Naturalidade: Naturalidade.value.originalValue.trim(),
-            Profissao: Profissao.value.originalValue.trim(),
             Telefone: telefone.value.originalValue.trim(),
-            DataExpedicao: dataExpedicao.value.originalValue.trim(),
-            OrgaoExpedidor: orgaoExpedidor.value.originalValue.trim(),
-            RG: rg.value.originalValue.trim(),
-            UF: uf.value.originalValue.trim(),
             CondicaoMedica: CondicaoMedica.value.originalValue.trim(),
             Acompanhamento: Acompanhamento.value.originalValue.trim(),
         };
